429ab226fdb65b9b540158717b3a4b5fb01dff52
Ezra Silvera <ezra@il.ibm.com>
2023-09-12 12:50:54 +0300

    regenerate files
    Signed-off-by: Ezra Silvera <ezra@il.ibm.com>

--- pkg/client/clientset/versioned/fake/register.go
+++ pkg/client/clientset/versioned/fake/register.go
@@ -19,13 +19,14 @@ limitations under the License.
 package fake
 
 import (
-	edgev1alpha1 "github.com/kubestellar/kubestellar/pkg/apis/edge/v1alpha1"
-	metav1alpha1 "github.com/kubestellar/kubestellar/pkg/apis/meta/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+
+	edgev1alpha1 "github.com/kubestellar/kubestellar/pkg/apis/edge/v1alpha1"
+	metav1alpha1 "github.com/kubestellar/kubestellar/pkg/apis/meta/v1alpha1"
 )
 
 var scheme = runtime.NewScheme()
